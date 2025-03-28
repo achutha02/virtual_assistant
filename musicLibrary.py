7201e9923490b0f951eed9abda6eaf8c983ca223
achutha02 <bidarahalliachyuta@gmail.com>
2025-03-28 08:25:26 +0530

    initial commit

---
+++ musicLibrary.py
@@ -0,0 +1,6 @@
+music={
+    "skyfall": "https://www.youtube.com/watch?v=DeumyOzKqgI&pp=ygUHc2t5ZmFsbNIHCQm9AIO1pN6f1A%3D%3D",
+    "satyatmaru": "https://www.youtube.com/watch?v=cE_Er98jIAQ&pp=ygUbZ3VydSBzYXR5YXRtYXJhIHBhZGFrZSBzb25n",
+    "give me some sunshine": "https://www.youtube.com/watch?v=lbCRtrrMvSw&pp=ygUaZ2l2ZSBtZSBzb21lIHN1bnNoaW5lIHNvbmc%3D",
+    "rcb": "https://www.youtube.com/watch?v=WOZSI2_m-3o&pp=ygUSdGVhbSBzaWRlIGZlYXQgcmNi"
+}
